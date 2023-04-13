57d25b9e0010039e8211bbd844865299d4462fc6
ngntrgduc <trungducnguyen1511@gmail.com>
2023-04-13 23:56:27 +0700

    Remove sort axis option

--- visualimiss/utils.py
+++ visualimiss/utils.py
@@ -1,23 +1,15 @@
 import numpy as np
 
-def nullity_sort(df, sort=None, axis='rows'):
+def nullity_sort(df, sort=None):
     
     if sort is None:
         return df
     elif sort not in ['asc', 'desc']:
-        raise ValueError("The `sort` parameter must be set to 'asc' or 'desc'.")
+        raise ValueError('The `sort` parameter must be set to "asc" or "desc".')
     
-    if axis not in ['rows', 'columns']:
-        raise ValueError("The `axis` parameter must be set to 'rows' or 'columns'.")
+    values = df.count(axis='rows').values
     
-    values = df.count(axis=axis).values
-    if axis == 'columns':
-        if sort == 'asc':
-            return df.iloc[np.argsort(values), :]
-        elif sort == 'desc':
-            return df.iloc[np.flipud(np.argsort(values)), :]
-    elif axis == 'rows':
-        if sort == 'asc':
-            return df.iloc[:, np.argsort(values)]
-        elif sort == 'desc':
-            return df.iloc[:, np.flipud(np.argsort(values))]
+    if sort == 'asc':
+        return df.iloc[:, np.argsort(values)]
+    elif sort == 'desc':
+        return df.iloc[:, np.flipud(np.argsort(values))]
